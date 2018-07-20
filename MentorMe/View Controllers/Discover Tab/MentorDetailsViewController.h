b157ec3154496e4dc54a398c51bfb6b731351362
Nico Salinas <nsalinas@fb.com>
2018-07-19 17:25:08 -0700

    reorganizing files

---
+++ MentorMe/View Controllers/Discover Tab/MentorDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MentorDetailsViewController.h
+//  MentorMe
+//
+//  Created by Nico Salinas on 7/19/18.
+//  Copyright © 2018 Taylor Murray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MentorDetailsViewController : UIViewController
+
+@end
---
+++ MentorMe/View Controllers/Discover Tab/MentorDetailsViewController.m
@@ -0,0 +1,37 @@
+//
+//  MentorDetailsViewController.m
+//  MentorMe
+//
+//  Created by Nico Salinas on 7/19/18.
+//  Copyright © 2018 Taylor Murray. All rights reserved.
+//
+
+#import "MentorDetailsViewController.h"
+
+@interface MentorDetailsViewController ()
+
+@end
+
+@implementation MentorDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
