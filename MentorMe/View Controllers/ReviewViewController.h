52d874d80ff699426b7efb774e4f0b1ed7193ae8
Taylor Murray <murratay@umich.edu>
2018-07-24 15:57:38 -0700

    added review view controllers and also rate files

---
+++ MentorMe/View Controllers/ReviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReviewViewController.h
+//  MentorMe
+//
+//  Created by Taylor Murray on 7/24/18.
+//  Copyright © 2018 Taylor Murray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReviewViewController : UIViewController
+
+@end
---
+++ MentorMe/View Controllers/ReviewViewController.m
@@ -0,0 +1,37 @@
+//
+//  ReviewViewController.m
+//  MentorMe
+//
+//  Created by Taylor Murray on 7/24/18.
+//  Copyright © 2018 Taylor Murray. All rights reserved.
+//
+
+#import "ReviewViewController.h"
+
+@interface ReviewViewController ()
+
+@end
+
+@implementation ReviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
