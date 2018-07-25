ab552dd3a2833be332cb91c63baa9eb660642f91
Taylor Murray <murratay@umich.edu>
2018-07-25 13:44:53 -0700

    reviews Buttons make array

--- MentorMe/View Controllers/ReviewViewController.m
+++ MentorMe/View Controllers/ReviewViewController.m
@@ -10,10 +10,32 @@
 
 @interface ReviewViewController () <UITextViewDelegate>
 @property (strong, nonatomic) IBOutlet UITextView *commentsTextView;
-
+@property (strong, nonatomic) NSArray *complimentsArray;
 @end
 
 @implementation ReviewViewController
+
+- (IBAction)touch1:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    
+}
+
+- (IBAction)touch2:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    
+}
+- (IBAction)touch3:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    
+}
+- (IBAction)touch4:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    
+}
+- (IBAction)touch5:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    
+}
 
 - (void)textViewDidBeginEditing:(UITextView *)textView{
     [UIView beginAnimations:nil context:NULL];
@@ -22,6 +44,15 @@
     offset.y += 280; // You can change this, but 200 doesn't create any problems
     [self.scrollView setContentOffset:offset];
     [UIView commitAnimations];
+}
+- (IBAction)doneAction:(UIButton *)sender {
+    NSMutableArray *mutableCompliments = [[NSMutableArray alloc]init];
+    [mutableCompliments addObject:[NSNumber numberWithBool:self.greatConvoButton.isSelected]];
+    [mutableCompliments addObject:[NSNumber numberWithBool:self.downToEarthButton.isSelected]];
+    [mutableCompliments addObject:[NSNumber numberWithBool:self.usefulAdviceButton.isSelected]];
+    [mutableCompliments addObject:[NSNumber numberWithBool:self.friendlyButton.isSelected]];
+    [mutableCompliments addObject:[NSNumber numberWithBool:self.superKnowledgeButton.isSelected]];
+    self.complimentsArray = [NSArray arrayWithArray:mutableCompliments];
 }
 
 - (void)viewDidLoad {
@@ -51,15 +82,15 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)tappedOutsdie:(UITapGestureRecognizer *)sender {
-    [self.commentsTextView resignFirstResponder];
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationDuration:0.35f];
-    CGPoint offset = self.scrollView.contentOffset;
-    offset.y -= 280; // You can change this, but 200 doesn't create any problems
-    [self.scrollView setContentOffset:offset];
-    [UIView commitAnimations];
-}
+//- (IBAction)tappedOutsdie:(UITapGestureRecognizer *)sender {
+//    [self.commentsTextView resignFirstResponder];
+//    [UIView beginAnimations:nil context:NULL];
+//    [UIView setAnimationDuration:0.35f];
+//    CGPoint offset = self.scrollView.contentOffset;
+//    offset.y -= 280; // You can change this, but 200 doesn't create any problems
+//    [self.scrollView setContentOffset:offset];
+//    [UIView commitAnimations];
+//}
 
 /*
 #pragma mark - Navigation
